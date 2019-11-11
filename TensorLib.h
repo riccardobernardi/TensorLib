3ae4d715c4b2e3791cf666758df148e1c2c87851
Davide Cecchini <davidececchini97@gmail.com>
2019-11-11 19:53:36 +0100

    NEW: created set() method in static-rank-tensor, FIX: added assertions in static-rank-tensor methods (slice, flatten, window), added data vector creation in constructor with widths

--- TensorLib.h
+++ TensorLib.h
@@ -20,11 +20,15 @@ public:
     // when the rank is not specified
     Tensor<T>(std::initializer_list<size_t>&& a){
         // cout << "costruttore : Tensor<T>(std::initializer_list<size_t>&& a)" << endl;
+        //TODO decidere se rank può essere 0
         if (rank!=0){
             assert(a.size()==rank);
         }
+
         widths = a;
         strides = cummult<size_t>(widths,1);
+        data = std::make_shared<std::vector<T>>(strides[0] * width[0], 0); //vettore lungo mult(width) di zeri
+
     }
 
     // when the rank is specified
@@ -53,47 +57,81 @@ public:
         }
     }
 
-    T operator()(initializer_list<size_t> a){
-        assert(a.size() == widths.size());
+    T operator()(initializer_list<size_t> indices){
+        assert(indices.size() == widths.size());
 
-        std::vector<size_t> b = a;
+        std::vector<size_t> indices_v = indices;
         size_t tmp = 0;
-        for(size_t i=0; i< b.size(); ++i){
-            assert(b[i] < widths[i] && b[i] >= 0);
-            tmp += b[i] * strides[i];
+        for(size_t i=0; i< indices_v.size(); ++i){
+            assert(indices_v[i] < widths[i] && indices_v[i] >= 0);
+            tmp += indices_v[i] * strides[i];
         }
 
         tmp += offset;
 
         return (data.get()->at(tmp));
+        //return (*data)[tmp];      //versione alternativa
+    }
+
+    void set(initializer_list<size_t> indices, T& value){
+        assert(indices.size() == widths.size());
+
+        std::vector<size_t> indices_v = indices;
+        size_t tmp = 0;
+        for(size_t i=0; i< indices_v.size(); ++i){
+            assert(indices_v[i] < widths[i] && indices_v[i] >= 0);
+            tmp += indices_v[i] * strides[i];
+        }
+
+        tmp += offset;
+
+        (*data)[tmp] = value;
     }
 
     Tensor<T> slice(const size_t&  index, const size_t& value){
+        assert(index >= 0 && index < width.size());
+        asser(value >= 0 && value < width[index]);
+        
+        if(rank == 1){
+            //TODO caso particolare in cui si crea un tensore di rango 0
+        }
+        
         Tensor<T> a = Tensor<T>(widths);
+        /*
+        tmp_widths = erase<size_t>(widths, index);
+        Tensor<T, rank-1> a = Tensor<T, rank-1>(tmp_widths);         //versione inefficiente ma corretta, crea un vettore vuoto 
+        a.strides = erase<size_t>(strides, index);                   // il problema di questa è che se nel costruttore controllo che (*data).size() sia conforme con width, 
+        a.offset += (strides[index] * value);                        //mi becco un errore perchè facendo lo slice ho degli elementi nel vettore che non c'entrano, quindi (*data).size()>mult(width)
+        a.data = data;                                               // con l'altra versione ho dubbi: non sfruttiamo il fatto che sappiamo il rank a tempo di compilazione
+        */
         a.widths = erase<size_t>(widths, index);
         a.strides = erase<size_t>(strides, index);
         a.offset += (strides[index] * value);
         return a;
     }
 
-    Tensor<T> flatten(const size_t& start, const size_t& stop){
+    Tensor<T> flatten(const size_t& start, const size_t& stop){  //estremi inclusi
+        assert(start >= 0 && start < width.size());
+        assert(stop >= 0 && stop < width.size());
         std::vector<size_t> new_width;
         size_t tmp=1;
-        size_t flat=-1;
+
+        if ( (rank - (stop - start +1)) == 0 ){
+            //TODO caso in cui il risultante è un tensore di grado 0
+        }
 
         for(int i=0;i<widths.size();++i){
-            if (i<start || i>stop){
+            if (i<start || i>stop) {
                 new_width.push_back(widths[i]);
-            }else{
+            } else {
                 tmp*=widths[i];
                 if(i==stop){
                     new_width.push_back(tmp);
-                    // flat = new_width.size() - 1;
                 }
             }
         }
 
-        Tensor<T> a = Tensor<T>(new_width);
+        Tensor<T> a = Tensor<T>(new_width); //qui non conosciamo il rank a tempo di compilazione perchè dipende da start e width
 
         // TODO opt
         a.strides = cummult(new_width);
@@ -106,6 +144,8 @@ public:
         assert(stop > start);
         assert(widths[index] > stop);
         assert(start >= 0);
+
+        //TODO gestire caso in cui genero un tensore di rank 0
 
         Tensor<T> a = Tensor<T>(widths);
 
